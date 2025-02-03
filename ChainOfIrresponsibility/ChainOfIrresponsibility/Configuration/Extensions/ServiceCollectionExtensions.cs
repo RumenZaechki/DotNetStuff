7419cf9c5b45bac433467e1f336c8826ee9bab41
Rumen Zaechki <mation_palaeo@aleeas.com>
2025-02-03 16:47:49 +0200

    Added the option to define service lifetime to the chain and chain builder

--- ChainOfIrresponsibility/ChainOfIrresponsibility/Configuration/Extensions/ServiceCollectionExtensions.cs
+++ ChainOfIrresponsibility/ChainOfIrresponsibility/Configuration/Extensions/ServiceCollectionExtensions.cs
@@ -1,16 +1,21 @@
-﻿using ChainOfIrresponsibility.Abstractions;
+﻿using System.Reflection.Metadata.Ecma335;
+using ChainOfIrresponsibility.Abstractions;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
 
 namespace ChainOfIrresponsibility.Configuration.Extensions
 {
     public static class ServiceCollectionExtensions
     {
-        public static IChainBuilder<T> AddChain<T>(this IServiceCollection services) where T : class
+        public static IChainBuilder<T> AddChain<T>(this IServiceCollection services, 
+        ServiceLifetime builderServiceLifetime = ServiceLifetime.Singleton, 
+        ServiceLifetime chainServiceLifetime = ServiceLifetime.Scoped) 
+        where T : class
         {
             IChainBuilder<T> builder = new ChainBuilder<T>(services);
-
-            services.AddSingleton(builder);
-            services.AddTransient(_ => builder.Build());
+            
+            services.Add(new ServiceDescriptor(typeof(IChainBuilder<T>), _ => builder,  builderServiceLifetime));
+            services.Add(new ServiceDescriptor(typeof(T), _ => builder.Build(),  chainServiceLifetime));
 
             return builder;
         }
